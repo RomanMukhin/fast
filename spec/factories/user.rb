bd07bb345fc48aa2408b2681a52aaf69893cb422
Roman Mukhin <roman11mukhin@gmail.com>
2013-03-18 10:00:38 +0200

    Update user.rb

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   sequence(:proper_email) {|u| "something#{u}@gmail.com"}
 
-  factory :user do
-	  name                 {    Faker::Name.name          }
+  factory :user do 
+    name                 {    Faker::Name.name          }
     password             {    Faker::Lorem.characters(8)}
     password_confirmation{    |u|   u.password          }
     email                {    generate(:proper_email)   }
