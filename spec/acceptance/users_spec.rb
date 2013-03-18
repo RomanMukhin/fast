614f3af9f27943da4f6a63422dd136e569cba8ed
Roman Mukhin <roman11mukhin@gmail.com>
2013-03-18 09:59:48 +0200

    Update users_spec.rb

--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -3,7 +3,7 @@ require 'acceptance/acceptance_helper'
 feature "Admin can manage users" do
   background do
     @user  = FactoryGirl.create(:user) 
-  	@admin = FactoryGirl.create(:admin) 
+    @admin = FactoryGirl.create(:admin) 
   end
   
   scenario "Admin sees users" do
@@ -13,21 +13,21 @@ feature "Admin can manage users" do
   end
 
   scenario "Admin can see users' accounts" do
-  	sign_in @admin
-  	within("#index_user_#{@user.id}") do
-  	  click_link "Show"
+    sign_in @admin
+    within("#index_user_#{@user.id}") do
+      click_link "Show"
     end
     page.should have_content('User info')
-  	page.should have_content(@user.name)
-  	page.should have_content(@user.city)
+    page.should have_content(@user.name)
+    page.should have_content(@user.city)
   end
 
   scenario "Admin can destroy users" do
-  	sign_in @admin
-  	within("#index_user_#{@user.id}") do
-  	  click_link "Destroy"
+    sign_in @admin
+    within("#index_user_#{@user.id}") do
+      click_link "Destroy"
     end
-  	page.should have_content("User was successfully destroyed.")
-  	page.should_not have_content(@user.name)
+    page.should have_content("User was successfully destroyed.")
+    page.should_not have_content(@user.name)
   end
 end
